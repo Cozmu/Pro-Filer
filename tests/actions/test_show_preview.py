2595c537fd3a2d0719bfa93ba5c9254765f25dd5
Cozmu <www.jorgereis@gmail.com>
2023-07-05 20:39:11 -0300

    test: testes da função show_preview

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,20 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+
+def test_show_preview_without_content(capsys):
+    show_preview({"all_files": [], "all_dirs": []})
+    captured = capsys.readouterr()
+    assert captured.out == "Found 0 files and 0 directories\n"
+
+def test_show_preview_with_content(capsys):
+    context = {
+    "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+    "all_dirs": ["src", "src/utils"]
+    }
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Found 3 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+        )
