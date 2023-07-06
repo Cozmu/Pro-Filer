019876a2a657479e9f488899a08166df1592325f
Cozmu <www.jorgereis@gmail.com>
2023-07-05 21:05:59 -0300

    test: implementando mais um teste para show_preview com capacidade maxima de retorno

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -8,8 +8,8 @@ def test_show_preview_without_content(capsys):
 
 def test_show_preview_with_content(capsys):
     context = {
-    "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
-    "all_dirs": ["src", "src/utils"]
+        "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+        "all_dirs": ["src", "src/utils"]
     }
     show_preview(context)
     captured = capsys.readouterr()
@@ -18,3 +18,23 @@ def test_show_preview_with_content(capsys):
         "First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']\n"
         "First 5 directories: ['src', 'src/utils']\n"
         )
+
+
+def  test_show_preview_with_full_content(capsys):
+    context = {
+        "all_files": [
+            "src/__init__.py", "src/app.py", "src/utils/__init__.py", "src/arquivo.py", "src/index.py", "src/util.py"
+            ],
+        "all_dirs": [
+            "src", "src/utils", "src/services", "src/seeders", "src/middlewares", "src/controlllers"
+            ]
+    }
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Found 6 files and 6 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', "
+        "'src/utils/__init__.py', 'src/arquivo.py', 'src/index.py']\n"
+        "First 5 directories: ['src', 'src/utils', 'src/services', "
+        "'src/seeders', 'src/middlewares']\n"
+    )
