5192d9cd4206c8b48b9977b19289629826aeaf39
Cozmu <www.jorgereis@gmail.com>
2023-07-07 14:59:08 -0300

    refactor: refatorando test de show disk usage

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,6 +1,6 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
 from unittest.mock import Mock, patch
-import os
+from pro_filer.cli_helpers import _get_printable_file_path  # NOQA
 
 def test_show_disk_usage_with_content(capsys, tmp_path):
     output_path_one = tmp_path / 'app.py'
@@ -10,26 +10,17 @@ def test_show_disk_usage_with_content(capsys, tmp_path):
     output_path_two.touch()
     output_path_two.write_text('string')
 
-    context = {
-        "all_files": [
-            str(output_path_one),
-            str(output_path_two),
-        ]
-    }
-    mock__get_printable_file_path_one = Mock(return_value=str(output_path_one))
-    mock__get_printable_file_path_two = Mock(return_value=str(output_path_two))
+    # mock__get_printable_file_path_one = Mock(return_value=str(output_path_one))
+    # mock__get_printable_file_path_two = Mock(return_value=str(output_path_two))_get_printable_file_path
 
-    with ( 
-        patch("pro_filer.cli_helpers._get_printable_file_path", mock__get_printable_file_path_one),
-        patch("pro_filer.cli_helpers._get_printable_file_path", mock__get_printable_file_path_two)
-    ):
-        show_disk_usage(context)
+    with patch("pro_filer.actions.main_actions._get_printable_file_path", lambda x:x):
+        show_disk_usage(context= {"all_files": [str(output_path_one), str(output_path_two)]})
 
 
     captured = capsys.readouterr()
     assert captured.out == (
-        "'/tmp/pytest-of-usuario/pyte...w_disk_usage_with_cont0/app.py':        19 (76%)\n"
-        "'/tmp/pytest-of-usuario/pyte...k_usage_with_cont0/__init__.py':        6 (24%)\n"
+        f"'{output_path_one}': 19 (76%)\n"
+        f"'{output_path_two}': 6 (24%)\n"
         "Total size: 25\n"
     )
 
